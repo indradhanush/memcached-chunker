e4a3141c2a89b9f515005545547b13925b5d4e3c
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-07-14 13:45:36 -0400

    Add test for set_file and fix a bug

--- chunker/tests.py
+++ chunker/tests.py
@@ -2,8 +2,11 @@
 Tests for Memcache chunker
 """
 
+# System imports
+import tempfile
 from unittest.case import TestCase
 
+# Local imports
 from chunker.client import Chunker
 
 
@@ -16,6 +19,17 @@ class ChunkerTestCase(TestCase):
 
     def test__set_chunks(self):
         self.chunker._set_chunks('test', self.test_string)
+
+        self.assertEqual(self.chunker.client.get('test-0'), 'aaaaaaaaaa')
+        self.assertEqual(self.chunker.client.get('test-1'), 'bbbbbbbbbb')
+        self.assertEqual(self.chunker.client.get('test-2'), 'cccccccccc')
+        self.assertEqual(self.chunker.client.get('test-3'), 'dddddd')
+
+    def test_set_file(self):
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(self.test_string)
+            f.flush()
+            self.chunker.set_file('test', f.name)
 
         self.assertEqual(self.chunker.client.get('test-0'), 'aaaaaaaaaa')
         self.assertEqual(self.chunker.client.get('test-1'), 'bbbbbbbbbb')
